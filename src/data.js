ab0404c1aa3d22e5e6da4068e26fd2627003f1e4
Sina Setayesh <s.setayesh@gmail.com>
2020-04-12 21:15:31 +0430

    firt commit

---
+++ src/data.js
@@ -0,0 +1,40 @@
+export const storeProducts = [
+  {
+    id: 1,
+    title: "Sina",
+    img: "logo.png",
+    price: 10,
+    company: "google",
+    inCart: false,
+    count: 0,
+    total: 0,
+    info: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras pulvinar accumsan pretium. Curabitur dapibus maximus velit. Mauris vestibulum est eu velit dignissim sagittis quis nec diam. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Etiam consectetur enim vel cursus dictum. Duis elementum purus vehicula mattis ultrices. Fusce rhoncus nunc lacus, vitae malesuada tellus malesuada sit amet. Nullam hendrerit nibh vitae auctor feugiat. Donec sit amet dui eros. Ut congue euismod purus, ut commodo dolor cursus sed.
+
+    Quisque commodo ac arcu eget ullamcorper. Nullam accumsan odio purus, nec lacinia mauris convallis nec. Aliquam vehicula semper iaculis. Aenean imperdiet ac arcu nec mollis. Nunc eget congue nulla. Sed est orci, placerat quis pulvinar vitae, imperdiet posuere massa. Duis non consectetur lectus, id ultricies justo. Pellentesque eget feugiat dolor. In eget nisi augue. Nullam volutpat id nulla sit amet volutpat. Vivamus id porttitor ante.`,
+  },
+  {
+    id: 2,
+    title: "Ali",
+    img: "logo.png",
+    price: 23,
+    company: "bpc",
+    inCart: false,
+    count: 0,
+    total: 0,
+    info: "test info of asda",
+  },
+];
+
+export const detailProduct = {
+  id: 2,
+  title: "Ali",
+  img: "logo.png",
+  price: 23,
+  company: "asda",
+  inCart: false,
+  count: 0,
+  total: 0,
+  info: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras pulvinar accumsan pretium. Curabitur dapibus maximus velit. Mauris vestibulum est eu velit dignissim sagittis quis nec diam. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Etiam consectetur enim vel cursus dictum. Duis elementum purus vehicula mattis ultrices. Fusce rhoncus nunc lacus, vitae malesuada tellus malesuada sit amet. Nullam hendrerit nibh vitae auctor feugiat. Donec sit amet dui eros. Ut congue euismod purus, ut commodo dolor cursus sed.
+
+  Quisque commodo ac arcu eget ullamcorper. Nullam accumsan odio purus, nec lacinia mauris convallis nec. Aliquam vehicula semper iaculis. Aenean imperdiet ac arcu nec mollis. Nunc eget congue nulla. Sed est orci, placerat quis pulvinar vitae, imperdiet posuere massa. Duis non consectetur lectus, id ultricies justo. Pellentesque eget feugiat dolor. In eget nisi augue. Nullam volutpat id nulla sit amet volutpat. Vivamus id porttitor ante.`,
+};
